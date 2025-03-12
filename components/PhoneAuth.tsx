8a05b45c73205cd44f4e01b104af64ac9ae29ce3
root <afurkankurt@outlook.com>
2025-03-12 07:03:58 +0300

    commit

--- components/PhoneAuth.tsx
+++ components/PhoneAuth.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { auth } from '@/lib/firebase'
+import { auth } from '@/firebase/config'
 import { 
   RecaptchaVerifier, 
   signInWithPhoneNumber,
